9ac52b8d6fec94cf0767436c2cc8e4dbd20c4a8d
Markswell <osoromarkswell80@gmail.com>
2023-09-18 12:42:48 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Markswell Ogutu';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + 'MARKSWELL OGUTU' + '!';
+const shortGreeting = "Welcome, " + 'M' + '!';
